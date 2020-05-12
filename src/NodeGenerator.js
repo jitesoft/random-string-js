2667431dab20ad0a6bfbda0ef3605f11a846f53c
Johannes Tegnér <johannes@jitesoft.com>
2020-05-12 09:16:23 +0200

    Fixed an issue where the buffer in nodegenerator did not return the correct type. Updated packages to latest versions.

--- src/NodeGenerator.js
+++ src/NodeGenerator.js
@@ -3,6 +3,6 @@ import { randomBytes } from 'crypto';
 
 export default class NodeGenerator extends Generator {
   getRandom (len) {
-    return randomBytes(len);
+    return new Uint8Array(randomBytes(len).buffer);
   }
 }
